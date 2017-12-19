b375a3c0c018bac459aa9def984de502bac8ddad
Daniel Lee <dlee@instructure.com>
2017-12-19 11:00:45 -0700

    Don't check types for firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
-import * as firebase from 'firebase'
+const firebase = require('firebase')
+
 const config = {
   apiKey: "AIzaSyDXUTybo8UepSVtRxitt7y4rvTDTOAldaE",
   authDomain: "wedding-site-a9f86.firebaseapp.com",

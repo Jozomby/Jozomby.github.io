3a0dc35017c47d063fe8c0219afba60c072ac2f7
Daniel Lee <dlee@instructure.com>
2017-12-18 20:08:55 -0700

    Switch photo location

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,7 +6,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <img src="./starwars.jpg" style={{'height':'100%', 'width':'100%'}}/>
+        <img src="http://noahfineart.com/wp-content/uploads/2015/07/Star_Wars_header_v4.jpg" style={{'height':'100%', 'width':'100%'}}/>
       </div>
     )
   }

8e18c57fa9521dadfa30faf288448e962ddbca89
Daniel Lee <dlee@instructure.com>
2017-12-30 10:50:53 -0700

    FAQ page

--- src/Page4.tsx
+++ src/Page4.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react'
+import './Page4.css'
 
 export default class Page4 extends React.Component {
   render() {
     return (
-      <h1>Page 4</h1>
+      <div className="Page4">
+        <h1>FAQs</h1>
+        <h3>How long have you known each other?</h3>
+        <p>We first met in March 2015, so it's been about 3 years.</p>
+        <h3>Do I need to bring a gift?</h3>
+        <p>No.  But we won't complain if you decide to.</p>
+        <h3>Can I come to the reception?</h3>
+        <p>Ummm, yes. That's what this website is for.</p>
+        <h3>When are you going to have kids?</h3>
+        <p>None of your business.</p>
+        <h3>How many kids are you going to have?</h3>
+        <p>Still none of your business.</p>
+        <h3>How did you meet?</h3>
+        <p>In person.</p>
+        <h3>How did he propose?</h3>
+        <p>Also in person. This time with a ring. There wasn't a ring when we first met.</p>
+        <h3>Did he wait for her on her mission?</h3>
+        <p>Define waiting.</p>
+        <h3>What's your favorite color?</h3>
+        <p>Teal. Duh.</p>
+        <h3>How did you know he/she was the one?</h3>
+        <p>See answer to question #5.</p>
+        <h3>Is there anything useful in this FAQ page?</h3>
+        <p>No.</p>
+      </div>
     )
   }
 }
